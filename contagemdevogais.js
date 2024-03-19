51bef03553f6e6753ec8c5aa73453ec7d849d817
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2024-03-19 17:14:31 -0300

    Exemplo 2

--- contagemdevogais.js
+++ contagemdevogais.js
@@ -1,2 +1,4 @@
-let contador = ["aula"]
+let vogais = ('a', 'b', 'c', 'd', 'e');
+
+let consoantes = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z');
 
